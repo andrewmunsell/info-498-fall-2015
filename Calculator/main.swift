e7d82595712cef75b37ac8a85434bf87fdbab433
Andrew Munsell <andrew@wizardapps.net>
2015-10-06 17:59:38 -0700

    Initial Commit

---
+++ Calculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by Andrew Munsell on 10/6/15.
+//  Copyright © 2015 Andrew Munsell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
