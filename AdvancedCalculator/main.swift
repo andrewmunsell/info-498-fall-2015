ce50d16f25b910af709c7a90d6d95fdff7eaafe0
Andrew Munsell <andrew@wizardapps.net>
2015-10-13 13:33:21 -0700

    Initial Commit

---
+++ AdvancedCalculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdvancedCalculator
+//
+//  Created by Andrew Munsell on 10/13/15.
+//  Copyright © 2015 Andrew Munsell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
